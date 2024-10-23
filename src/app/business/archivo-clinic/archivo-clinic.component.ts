cd1e15cb11e56cd780689e9ce70efab8ae8a3070
ALEXSTR19 <alex129t@outlook.com>
2024-10-23 14:20:01 -0500

    Logro: se agrega formulario a archivo-clinic component

--- src/app/business/archivo-clinic/archivo-clinic.component.ts
+++ src/app/business/archivo-clinic/archivo-clinic.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-archivo-clinic',
@@ -8,5 +9,25 @@ import { Component } from '@angular/core';
   styleUrl: './archivo-clinic.component.css'
 })
 export default class ArchivoClinicComponent {
+  tabla_archive: any;
+  constructor(){}
 
-}
+  form: FormGroup = new FormGroup
+  ({
+   expediente: new FormControl('', Validators.required),
+    nombre: new FormControl('', Validators.required),
+    ap_paterno: new FormControl('', Validators.required),
+    ap_materno: new FormControl('', Validators.required),
+    fecha: new FormControl('', Validators.required),
+    elabora: new FormControl('', Validators.required),
+    matri: new FormControl('', Validators.required),
+  });
+  
+  agregarData(){
+    
+      }  
+      resetForm(){
+        this.form.reset();
+      }
+    }
+
